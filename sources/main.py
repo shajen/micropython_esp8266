01585c91247526a90b34a2aed546f52b03c852a3
Borys Dyczko <shajen@shajen.pl>
2017-10-12 22:36:05 +0200

    Add on/off display backlight, changed i2c pinout.

--- sources/main.py
+++ sources/main.py
@@ -3,6 +3,7 @@ from machine import Pin, I2C, Timer
 from helper import syncDatetime, printDebug
 import devices
 import display
+import utime
 
 printDebug("NODEMCU", "boot up")
 syncDatetime()
@@ -13,9 +14,13 @@ _display = display.Display(i2c, _devices)
 
 def update(timer):
     _devices.update()
+    hour = utime.localtime(utime.time())[3]
+    _display.setBacklight(7 <= hour and hour <= 22)
     _display.update()
 
 tim1 = Timer(0)
 tim1.init(period=1000, mode=Timer.PERIODIC, callback=update)
 tim2 = Timer(1)
 tim2.init(period=60000, mode=Timer.PERIODIC, callback=lambda t: _devices.upload())
+tim3 = Timer(2)
+tim3.init(period=600000, mode=Timer.PERIODIC, callback=lambda t: syncDatetime())
