32661c6be740f8b8286bac78c133633aeee5f8f9
shajen <shajen@shajen.pl>
2018-12-07 23:55:32 +0100

    Uploading compiled version of scripts.

--- scripts/micro_remote_file.sh
+++ scripts/micro_remote_file.sh
@@ -6,11 +6,12 @@ PORT=8266
 
 [[ -z "$1" ]] && echo "File $1 not found." && exit 0
 
-if [[ $1 == *.py ]] && [[ $1 != boot.py ]] && [[ $1 != main.py ]]
+NAME=$(basename $1)
+if [[ $NAME == *.py ]] && [[ $NAME != boot.py ]] && [[ $NAME != main.py ]] && [[ $NAME != webrepl_cfg.py ]]
 then
 	#/home/shajen/micropython/micropython/mpy-cross/mpy-cross -O2 -mcache-lookup-bc $1
-	#FILE="${1%.py}.mpy"
-	FILE=$1
+	/home/shajen/git/micropython-firmware/mpy-cross/mpy-cross -O3 $1
+	FILE="${1%.py}.mpy"
 else
 	FILE=$1
 fi
