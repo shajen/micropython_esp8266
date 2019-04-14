51cb791fa23b1f1f4e3ce34142e46942b0b2d93c
shajen <shajen@shajen.pl>
2019-04-14 13:24:40 +0200

    Fixed animator.

--- sources/projects/animator/main.py
+++ sources/projects/animator/main.py
@@ -5,23 +5,17 @@ import server
 import status_server_controller
 import utils
 
-utils.printLog("NODEMCU", "animator boot up")
-
+utils.printLog("ANIMATOR", "boot up")
+utils.createSyncDateTimeTimer()
 animatorServerController = animator_server_controller.AnimatorServerController(machine.Pin(config.D4, machine.Pin.OUT))
 statusController = status_server_controller.StatusServerController('Animator', [animatorServerController])
-
-def timeoutTick(timer):
-    animatorServerController.tick()
-
-def timeout10minutes(timer):
-    utils.syncDatetime()
-
-timeout10minutes(None)
-
-tim1 = utils.timer()
-tim1.init(period=1, mode=machine.Timer.PERIODIC, callback=timeoutTick)
-tim3 = utils.timer()
-tim3.init(period=600000, mode=machine.Timer.PERIODIC, callback=timeout10minutes)
-
 _server = server.Server(config.SERVER_PORT, [statusController, animatorServerController])
-_server.run()
+
+try:
+    _server.run()
+except KeyboardInterrupt:
+    utils.printLog("ANIMATOR", "stopped by the user")
+    utils.deleteTimers()
+except Exception as e:
+    utils.printLog("ANIMATOR", "exception during server run: %s" % e)
+    machine.reboot()
