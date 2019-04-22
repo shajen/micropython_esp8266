0fb4514649b8bf62e571b8545ca90303bfa47abf
shajen <shajen@shajen.pl>
2019-04-22 21:08:24 +0200

    Refactored pin_scheduler to use mqtt instead server.

--- scripts/projects/micro_pin_scheduler.sh
+++ scripts/projects/micro_pin_scheduler.sh
@@ -11,7 +11,7 @@ eval $FLASH_COMMAND hardware/pin_scheduler.py
 eval $FLASH_COMMAND $ROOT_PATH/config
 eval $FLASH_COMMAND utils.py
 eval $FLASH_COMMAND hardware/temperature_sensor.py
-eval $FLASH_COMMAND network/server.py
+eval $FLASH_COMMAND network/mqtt_client.py
 eval $FLASH_COMMAND network/status_server_controller.py
 
 popd
--- sources/projects/pin_scheduler/main.py
+++ sources/projects/pin_scheduler/main.py
@@ -1,22 +1,24 @@
 import config
-import temperature_sensor
 import machine
+import mqtt_client
 import pin_scheduler
-import server
 import status_server_controller
+import temperature_sensor
 import utils
 
+_mqttClient = mqtt_client.MqttClient()
+utils.__LOG_CALLBACK = lambda level, label, message: _mqttClient.publishLog(level, label, message)
 utils.printInfo("PIN_SCHEDULER", "boot up")
-_temperature_sensor = temperature_sensor.TemperatureSensor(machine.Pin(config.DS18B20_PIN))
+_temperature_sensor = temperature_sensor.TemperatureSensor(machine.Pin(config.DS18B20_PIN), _mqttClient)
 pinScheduler = pin_scheduler.PinScheduler(machine.Pin(config.SCHEDULER_PIN, machine.Pin.OUT), [((19, 0, 0), 36)])
-statusController = status_server_controller.StatusServerController('Pin Scheduler', [])
-_server = server.Server(config.SERVER_PORT, [statusController])
+statusController = status_server_controller.StatusServerController(_mqttClient, 'Sonoff')
+_mqttClient.setControllers([statusController])
 
 try:
-    _server.run()
+    _mqttClient.run()
 except KeyboardInterrupt:
     utils.printInfo("PIN_SCHEDULER", "stopped by the user")
     utils.deleteTimers()
 except Exception as e:
     utils.printWarn("PIN_SCHEDULER", "exception during mqtt client run: %s" % e)
-    machine.reboot()
+    machine.reset()
